4df2e6bcc23c3292e7d559ef69e5bb331a4c3a15
vuhroon <VarunSinghX97@gmail.com>
2024-07-14 17:01:59 -0400

    added brewfile and monitor.sh

---
+++ monitors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if the specific USB device is connected
+DEVICE_NAME="Anker USB-C Universal Dock"
+DEVICE_CONNECTED=$(system_profiler SPUSBDataType | grep "$DEVICE_NAME")
+
+# Function to open the applications
+open_apps() {
+    open -a "BetterDisplay"
+    open -a "DisplayLink Manager"
+}
+
+# Function to close the applications
+close_apps() {
+    osascript -e 'quit app "BetterDisplay"'
+    osascript -e 'quit app "DisplayLink Manager"'
+}
+
+# If the device is found, open the applications
+if [[ -n "$DEVICE_CONNECTED" ]]; then
+    echo "$DEVICE_NAME is connected."
+    open_apps
+else
+    echo "$DEVICE_NAME is not connected."
+    close_apps
+fi
